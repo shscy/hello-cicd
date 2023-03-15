051286044cb676cf419e8ccc2c9825017f87c694
zuoxiaoliang <zuoxiaoliang@bituniverse.org>
2023-03-15 19:48:32 +0800

    init

--- c/cc.c
+++ c/cc.c
@@ -6,10 +6,10 @@ int main(){
     Event e;
     e.account_id = 12,
     e.strategy_id = 1;
-    e.amount = "123\0";
-    e.coin = "USDT\0";
+    e.amount = "123";
+    e.coin = "USDT";
     e.event_type = 0;
-    e.trace_id = "trace_id_0\0";
+    e.trace_id = "trace_id_0";
 
     write_db(&w, &e);
     close_db(&w);
